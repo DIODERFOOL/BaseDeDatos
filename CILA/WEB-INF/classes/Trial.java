9af84016eb2d2108887190d30707bf630872406c
Nicolás <nicogazzolo2398@hotmail.com>
2019-04-23 09:47:22 -0500

    Crear jucio, registrar jucio
    Modifique admins ya que no tenia la view de crar jucio, la clase Trial.java para trabajar el servlet y la view de crear el jucio

---
+++ CILA/WEB-INF/classes/Trial.java
@@ -0,0 +1,28 @@
+public class Trial{
+
+	public String address;
+	public String date;
+
+	public Trial(String address, String date){
+		this.address= address;
+		this.date=date;
+	}
+
+	public void setAddress(String address){
+		this.address = address;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+	public void setDate(String date){
+		this.date = date;
+	}
+
+	public String getDate(){
+		return date;
+	}
+
+
+}
