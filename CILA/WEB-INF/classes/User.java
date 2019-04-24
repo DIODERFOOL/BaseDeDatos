41b0329f897bd700bf9b39d50f373cdddaface1f
Rafael Rojas <yugnarcorp@hotmail.com>
2019-04-24 15:51:51 -0500

    Updated User to include IDs

--- CILA/WEB-INF/classes/User.java
+++ CILA/WEB-INF/classes/User.java
@@ -1,15 +1,25 @@
 public class User{
 	
+	private long userID;
 	private String name;
 	private String username;
 	private String password;
 	private String role;
 
-	public User(String name, String username, String password, String role){
+	public User(long userID, String name, String username, String password, String role){
+		this.userID = userID;
 		this.name = name;
 		this.username = username;
 		this.password = password;
 		this.role = role;
+	}
+
+	public void setUserID(long userID){
+		this.userID = userID;
+	}
+
+	public long getUserID(){
+		return userID;
 	}
 
 	public void setName(String name){
