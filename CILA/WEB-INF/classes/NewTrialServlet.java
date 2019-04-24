7bad4ae4deefae1eaf1c00c9725b2a573de423c9
Nicolás <nicogazzolo2398@hotmail.com>
2019-04-24 16:25:43 -0500

    Jucio V2.0
    Creacion del JSP de trial, Servlet de Trial ya terminado....creo,

--- CILA/WEB-INF/classes/NewTrialServlet.java
+++ CILA/WEB-INF/classes/NewTrialServlet.java
@@ -17,5 +17,63 @@ public class NewTrialServlet extends HttpServlet{
 		}	
 	}
 
-	
+	public void doPost(HttpServletRequest request, HttpServletResponse response){
+
+		try{
+
+			//------Connection to mySQL setup STARTS-------
+			//credentials
+			String dbbase = getServletContext().getInitParameter("base");
+			String dbusuario = getServletContext().getInitParameter("usuario");
+			String dbpassword = getServletContext().getInitParameter("pass");
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//general setup - driver setup, connection, statement setup
+			String url = "jdbc:mysql://localhost/"+dbbase+"?useSSL=false&allowPublicKeyRetrieval=true";
+			Connection con = DriverManager.getConnection(url,dbusuario,dbpassword);
+			Statement stat = con.createStatement();
+
+			//------Connection to mySQL setup ENDS----------
+
+			//------Trial creation STARTS------
+
+			//retrieve values from register's forms
+			String address = request.getParameter("addAddress");
+			String date = requestParameter("addDate");
+
+			//save values in the database
+			int res = stat.executeUpdate("insert into file(address, date) VALUES (\"" + address + "\", \"" + date + "\");");
+
+			//!!!!!!---------   DEBUGGING - For class presentation only - Creating a JSP with all registered users example !!!!!!---------
+
+			ResultSet res2 = stat.executeQuery("SELECT * FROM file;");
+			Vector<Trial> fileList = new Vector<Trial>();
+
+			while(res2.next()){
+				//Corregir formato de la fecha para impresión en el jsp
+				String reformatDate = res2.getString("creation_date");
+				//2018-mm-dd -> dd-mm-2018
+				String day = reformatDate.substring(8);
+				String month = reformatDate.substring(5,7);
+				String year =  reformatDate.substring(0,4);
+				reformatDate = day + "/" + month + "/" + year;
+				File aux = new File(res2.getString("name"), reformatDate);
+				fileList.add(aux);
+			}
+
+			stat.close();
+			con.close();
+
+			request.setAttribute("trialList",trialList);
+			RequestDispatcher disp =  getServletContext().getRequestDispatcher("/showRegisteredTrial.jsp");
+
+			if(disp!=null){
+				disp.forward(request,response);
+			}
+		}
+
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
