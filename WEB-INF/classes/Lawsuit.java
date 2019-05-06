987e4f65026f1d8d3572410e90dc5f9b1c382eee
Rafael Rojas <yugnarcorp@hotmail.com>
2019-05-05 23:05:19 -0500

    See Active Lawsuits

--- WEB-INF/classes/Lawsuit.java
+++ WEB-INF/classes/Lawsuit.java
@@ -1,45 +1,45 @@
 public class Lawsuit {
-	private String name;
-	private String affair;
-	private String adress;
-	private int trial_id;
+    private String name;
+    private String affair;
+    private String adress;
+    private int trial_id;
 
-	public Lawsuit(String name, String affair, String adress, int trial_id) {
-		this.name = name;
-		this.affair = affair;
-		this.adress = adress;
-		this.trial_id = trial_id;
-	}
+    public Lawsuit(String name, String affair, String adress, int trial_id) {
+        this.name = name;
+        this.affair = affair;
+        this.adress = adress;
+        this.trial_id = trial_id;
+    }
 
-	public void setName(String name){
-		this.name = name;
-	}
+    public void setName(String name){
+        this.name = name;
+    }
 
-	public String getName(){
-		return name;
-	}
+    public String getName(){
+        return name;
+    }
 
-	public void setAffair(String affair){
-		this.affair = affair;
-	}
+    public void setAffair(String affair){
+        this.affair = affair;
+    }
 
-	public String getAffair(){
-		return affair;
-	}
+    public String getAffair(){
+        return affair;
+    }
 
-	public void setAdress(String name){
-		this.adress = adress;
-	}
+    public void setAdress(String name){
+        this.adress = adress;
+    }
 
-	public String getAdress(){
-		return adress;
-	}
+    public String getAdress(){
+        return adress;
+    }
 
-	public void setTrialId(int trial_id) {
-		this.trial_id = trial_id;
-	}
+    public void setTrialId(int trial_id) {
+        this.trial_id = trial_id;
+    }
 
-	public int getTrialId() {
-		return trial_id;
-	}
+    public int getTrialId() {
+        return trial_id;
+    }
 }
